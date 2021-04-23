a986ae2a8799aed3ffeee7c93d51e402f30a6beb
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-04-23 13:14:28 -0300

    acomodacoes

--- desafio-2/index.php
+++ desafio-2/index.php
@@ -21,6 +21,60 @@
             </div>
         </div>
     </section>
+
+    <section class="acomodacoes">
+        <div class="acomodacoes-conteudo">
+            <h2 class="titulo">Nossas Acomodações</h2>
+            <ul class="lista">
+                <li class="item">
+                    <img src="public/img/suite-master.png" class="imagem" alt="Suíte Master">
+                    <div class="inferior">
+                        <div class="conteudo">
+                            <h3 class="subtitulo">Suíte Master</h3>
+                            <p class="texto">
+                                Cama de casal Queen, Frigobar
+                                Varanda com vista pro mar
+                                Banheiro individual
+                            </p>
+                            <a class="link" href="#">Reservar</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="item">
+                    <img src="public/img/suite-simples.png" class="imagem" alt="Suíte Simples">
+                    <div class="inferior">
+                        <div class="conteudo">
+                            <h3 class="subtitulo">Suíte Simples</h3>
+                            <p class="texto">
+                                Acomodação para duas pessoas (casal)
+                                ou 1 pessoa (quarto individual)
+                                Cama de casal Queen, Frigobar, 
+                                Banheiro individual
+                            </p>
+                            <a class="link" href="#">Reservar</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="item">
+                    <img src="public/img/quarto-coletivo.png" class="imagem" alt="Quarto Coletivo">
+                    <div class="inferior">
+                        <div class="conteudo">
+                            <h3 class="subtitulo">Quarto Coletivo</h3>
+                            <p class="texto">
+                                Acomodação coletiva para até 6
+                                pessoas 3 beliches, Frigobar Locker no
+                                quarto com 1 armazenamento
+                                para cada pessoa
+                            </p>
+                            <a class="link" href="#">Reservar</a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+
+            <a href="#" class="mais" title="Mais acomodações">Mais acomodações</a>
+        </div>
+    </section>
 </main>
 
 <?php include_once 'includes/footer.php'; ?>
