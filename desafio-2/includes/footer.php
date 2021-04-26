1f6cc6c8d7db11d916de245931bbe520638d7516
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-04-26 13:50:41 -0300

    começando rodape

--- desafio-2/includes/footer.php
+++ desafio-2/includes/footer.php
@@ -1,3 +1,58 @@
+<footer class="rodape">
+    <div class="imagem-fundo">
+        <img src="public/img/contato-fundo.png" alt="Contato | House & Hostel">
+    </div>
+    <div class="conteudo">
+
+        <h2 class="subtitulo">Fale conosco</h2>
+        <h1 class="titulo">
+            Reserve sua hospedagem<br />
+            em Guarujá
+        </h1>
+
+        <section class="informacoes">
+            <div class="informacao">
+                <div class="icon">
+                    <i class="fas fa-lg fa-phone-alt"></i>
+                </div>
+                <div class="conteudo">
+                    <h3 class="subtitulo">Atendimento</h3>
+                    <a href="tel:0000000000" class="link">Fone fax: (00)0000-0000</a>
+                    <a href="mailto:contato@house&hostel.com.br" class="link">contato@house&hostel.com.br</a>
+                    <div class="whatsapp">
+                        <i class="fab fa-whatsapp"></i>
+                        <a href="mailto:contato@house&hostel.com.br" class="link whatsapp-link">(00) 0000-000</a>
+                    </div>
+                </div>
+            </div>
+            <div class="informacao">
+                <div class="icon">
+                    <i class="fas fa-lg fa-map-marker-alt"></i>
+                </div>
+                <div class="conteudo">
+                    <h3 class="subtitulo">Endereço</h3>
+                    <p class="texto">
+                        Avenida Miguel Stéfano, 4923<br />
+                        Enseada, Guarujá.
+                    </p>
+                </div>
+            </div>
+            <div class="informacao">
+                <div class="icon">
+                    <i class="far fa-lg fa-clock"></i>
+                </div>
+                <div class="conteudo">
+                    <h3 class="subtitulo">Funcionamento</h3>
+                    <p class="texto">
+                        Segunda à Sexta das 10h00 às 18h00<br />
+                        Sábado das 09h00 às 13h00
+                    </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</footer>
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="public/js/main-min.js"></script>
