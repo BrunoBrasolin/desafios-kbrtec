8a40b53848c49348a9b1d67da217d6ae1aa9d51a
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-04-27 09:52:30 -0300

    base desktop

---
+++ desafio-2/includes/base.php
@@ -0,0 +1,7 @@
+<section class="base">
+    <div class="conteudo">
+        <p class="item">House & Hostel  - Todos os direitos reservados</p>
+
+        <a href="https://www.kbrtec.com.br/" target="_blank" class="item">Desenvolvido por KBRTEC</a>
+    </div>
+</section>
--- desafio-2/includes/footer.php
+++ desafio-2/includes/footer.php
@@ -48,7 +48,9 @@
             </div>
         </section>
     </div>
+
 </footer>
+<?php include_once 'base.php'; ?>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
