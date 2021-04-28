eb1503e1254ec0a84e713bec7f95bd6ef4687fc1
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-04-28 12:37:57 -0300

    fix config

--- desafio-2/config.php
+++ desafio-2/config.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($_SERVER['SERVER_NAME'] == 'http://ambiente-desenvolvimento.provisorio.ws/') {
+if ($_SERVER['SERVER_NAME'] == 'ambiente-desenvolvimento.provisorio.ws') {
     $GLOBALS['home_url'] = 'http://' . $_SERVER['SERVER_NAME'] . '/bruno-brasolin/desafio-2';
 } else {
     $GLOBALS['home_url'] = 'http://' . $_SERVER['SERVER_NAME'] . ':8001';
