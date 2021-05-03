91f5761d93adb3ed5436da99fdd436239e77663d
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-05-03 10:24:05 -0300

    404 page

---
+++ desafio-2/404.php
@@ -0,0 +1,11 @@
+<?php include_once 'includes/header.php'; ?>
+
+<main class="pagina-404">
+    <h2 class="titulo">
+        404
+    </h2>
+    <h3 class="subtitulo">Parece que não encontramos sua página</h3>
+    <a href="<?= $GLOBALS['home_url'] ?>" class="botao">Volte para a página inicial</a>
+</main>
+
+<?php include_once 'includes/footer.php'; ?>
