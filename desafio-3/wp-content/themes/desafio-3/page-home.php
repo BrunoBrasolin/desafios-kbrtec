9c3a8a12053304ddf81746e604abfb21c36c5e48
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-05-21 12:11:57 -0300

    secao encontre desktop

--- desafio-3/wp-content/themes/desafio-3/page-home.php
+++ desafio-3/wp-content/themes/desafio-3/page-home.php
@@ -4,7 +4,8 @@
   <section class="banner-item">
     <img src="<?= get_template_directory_uri() ?>/assets/dist/images/background-banner.png" alt="" class="background-image">
     <section class="col left">
-      <h2 class="welcome">Bem-vindo ao <strong>grupo sartori</strong></h2>
+      <h2 class="welcome">Bem-vindo ao <strong>grupo sartori</strong>
+      </h2>
       <h1 class="title">
         treinamento <br />
         recrutamento <br />
@@ -21,18 +22,21 @@
 
     <section class="col right">
       <span class="arrow banner-prev-arrow">
-        <i data-feather="chevron-left"></i>
+        <i data-feather="chevron-left">
+        </i>
       </span>
 
       <span class="arrow banner-next-arrow">
-        <i data-feather="chevron-right"></i>
+        <i data-feather="chevron-right">
+        </i>
       </span>
     </section>
   </section>
   <section class="banner-item">
     <img src="<?= get_template_directory_uri() ?>/assets/dist/images/background-banner.png" alt="" class="background-image">
     <section class="col left">
-      <h2 class="welcome">Bem-vindo ao <strong>grupo sartori</strong></h2>
+      <h2 class="welcome">Bem-vindo ao <strong>grupo sartori</strong>
+      </h2>
       <h1 class="title">
         treinamento2 <br />
         recrutamento2 <br />
@@ -53,9 +57,38 @@
       </span>
 
       <span class="arrow banner-next-arrow">
-        <i data-feather="chevron-right"></i>
+        <i data-feather="chevron-right">
+        </i>
       </span>
     </section>
+  </section>
+</section>
+
+<section class="encontre">
+  <section class="encontre-content">
+    <div class="top">
+      <span class="icon">
+        <i data-feather="search"></i>
+      </span>
+      <h2 class="title">Encontre vagas por área de especialidade</h2>
+    </div>
+
+    <form class="form">
+      <div class="input-group">
+        <label for="cargo" class="label">Cargo:</label>
+        <input type="text" name="cargo" id="cargo" class="input">
+      </div>
+      <div class="input-group">
+        <label for="cidade" class="label">Cidade:</label>
+        <input type="text" name="cidade" id="cidade" class="input">
+      </div>
+      <div class="input-group">
+        <label for="estado" class="label">Estado:</label>
+        <input type="text" name="estado" id="estado" class="input">
+      </div>
+
+      <button type="submit" class="button">Filtrar</button>
+    </form>
   </section>
 </section>
 
