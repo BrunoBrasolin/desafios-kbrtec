c18b0e031ddfff4676a9f8e7d8092f78dfe942d3
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-06-02 11:50:16 -0300

    sobre nos desktop

---
+++ desafio-3/wp-content/themes/desafio-3/templates/template-sobre.php
@@ -0,0 +1,203 @@
+<?php
+// Template name: Sobre nós
+?>
+
+<?php get_header(); ?>
+
+<main class="sobre-page">
+  <header class="header">
+    <img src="<?= get_template_directory_uri() ?>/assets/dist/images/grupo-sartori-icone.svg" alt="Grupo Sartori" title="Grupo Sartori">
+    <h1 class="title">Somos o grupo sartori</h1>
+    <p class="text">
+      Pra resumir em uma frase:</p>
+    <h2 class="subtitle">Somos aquela empresa que não busca apenas clientes e sim parceiros de negócios, nos processos estratégicos de RH.</h2>
+  </header>
+  <figure class="banner">
+    <img src="<?= get_template_directory_uri() ?>/assets/dist/images/sobre-background.png" alt="Grupo Sartori" title="Grupo Sartori">
+  </figure>
+
+  <section class="sobre-container">
+    <div class="sobre-content">
+      <div class="left">
+        <p class="text">
+
+          Uma empresa inovadora quando se fala em gestão de pessoas e todos os seus processos estratégicos que envolvem: Recrutamento & Seleção / Treinamento & desenvolvimento / Avaliações Psicológicas / Clima e Diagnóstico organizacional, esse é o nosso foco.
+        </p>
+        <p class="text">
+
+          A nossa consultoria conta com um forte viés tecnológico em todos os nossos processos, levando aos nossos clientes assertividade com retenção dos talentos, com um único objetivo: Maximizar os seus resultados com consistência e isso começa no nosso processo seletivo.
+        </p>
+        <p class="text">
+
+          Somos focados em empresas que realmente tenham esses mesmos valores e que buscam a mesma coisa que acreditamos.
+        </p>
+
+        <strong class="strong">Esse é o nosso DNA!</strong>
+
+        <div class="estrategia">
+          <img src="<?= get_template_directory_uri() ?>/assets/dist/images/objetivo-icone.png" alt="Grupo Sartori" title="Grupo Sartori">
+          <p class="estrategia-text">
+            Entre a estratégia e a execução com com o Grupo Sartori
+          </p>
+        </div>
+
+        <img class="dots" src="<?= get_template_directory_uri() ?>/assets/dist/images/dots.png" alt="Grupo Sartori" title="Grupo Sartori">
+      </div>
+
+      <div class="right">
+        <div class="item">
+          <figure class="icon">
+            <img src="<?= get_template_directory_uri() ?>/assets/dist/images/visao-icone.png" alt="Grupo Sartori" title="Grupo Sartori">
+          </figure>
+          <h3 class="item-title">Visão</h3>
+          <p class="text">
+            Ser a principal referência em Consultoria de RH do País até 2025.
+          </p>
+        </div>
+
+        <div class="item">
+          <figure class="icon">
+            <img src="<?= get_template_directory_uri() ?>/assets/dist/images/missao-icone.png" alt="Grupo Sartori" title="Grupo Sartori">
+          </figure>
+          <h3 class="item-title">Missão</h3>
+          <p class="text">
+            Ser a empresa que melhor entende e satisfaz as necessidades das empresas e candidatos, de forma humana, personalizada e com resultados consistentes.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="valores-container">
+    <div class="valores-content">
+      <h3 class="valores-title">Valores</h3>
+
+      <ul class="list">
+        <li class="item">
+          <figure class="icon">
+            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0)">
+                <path d="M13.7496 28.7304C6.16863 28.7304 0 22.562 0 14.981C0 7.39995 6.16863 1.23132 13.7496 1.23132C16.3858 1.23132 18.9506 1.98249 21.1668 3.40495C21.6019 3.68509 21.7294 4.26506 21.4492 4.70129C21.1682 5.13753 20.5893 5.26364 20.1531 4.98372C18.2406 3.7549 16.0258 3.10626 13.7496 3.10626C7.20223 3.10626 1.87494 8.43355 1.87494 14.981C1.87494 21.5282 7.20223 26.8554 13.7496 26.8554C20.2968 26.8554 25.6241 21.5282 25.6241 14.981C25.6241 14.5896 25.6054 14.2021 25.5692 13.821C25.5193 13.3047 25.8967 12.8472 26.4117 12.7973C26.9292 12.7497 27.3853 13.1248 27.4354 13.6398C27.4778 14.081 27.4991 14.5285 27.4991 14.981C27.4991 22.562 21.3307 28.7304 13.7496 28.7304Z" fill="black" />
+                <path d="M15.3118 18.1058C15.072 18.1058 14.8319 18.0145 14.6495 17.8309L9.02465 12.2061C8.65845 11.8397 8.65845 11.246 9.02465 10.8798C9.39085 10.5136 9.98455 10.5136 10.3507 10.8798L15.3132 15.8423L28.3989 2.75634C28.7653 2.39014 29.359 2.39014 29.7252 2.75634C30.0914 3.12254 30.0914 3.71624 29.7252 4.08244L15.9756 17.8321C15.7918 18.0145 15.5519 18.1058 15.3118 18.1058Z" fill="black" />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="30" height="30" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+
+          </figure>
+          <h4 class="item-title">Honestidade:</h4>
+          <p class="text">
+            Verdade acima de qualquer coisa.
+          </p>
+        </li>
+        <li class="item">
+          <figure class="icon">
+            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0)">
+                <path d="M13.7496 28.7304C6.16863 28.7304 0 22.562 0 14.981C0 7.39995 6.16863 1.23132 13.7496 1.23132C16.3858 1.23132 18.9506 1.98249 21.1668 3.40495C21.6019 3.68509 21.7294 4.26506 21.4492 4.70129C21.1682 5.13753 20.5893 5.26364 20.1531 4.98372C18.2406 3.7549 16.0258 3.10626 13.7496 3.10626C7.20223 3.10626 1.87494 8.43355 1.87494 14.981C1.87494 21.5282 7.20223 26.8554 13.7496 26.8554C20.2968 26.8554 25.6241 21.5282 25.6241 14.981C25.6241 14.5896 25.6054 14.2021 25.5692 13.821C25.5193 13.3047 25.8967 12.8472 26.4117 12.7973C26.9292 12.7497 27.3853 13.1248 27.4354 13.6398C27.4778 14.081 27.4991 14.5285 27.4991 14.981C27.4991 22.562 21.3307 28.7304 13.7496 28.7304Z" fill="black" />
+                <path d="M15.3118 18.1058C15.072 18.1058 14.8319 18.0145 14.6495 17.8309L9.02465 12.2061C8.65845 11.8397 8.65845 11.246 9.02465 10.8798C9.39085 10.5136 9.98455 10.5136 10.3507 10.8798L15.3132 15.8423L28.3989 2.75634C28.7653 2.39014 29.359 2.39014 29.7252 2.75634C30.0914 3.12254 30.0914 3.71624 29.7252 4.08244L15.9756 17.8321C15.7918 18.0145 15.5519 18.1058 15.3118 18.1058Z" fill="black" />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="30" height="30" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+
+          </figure>
+          <h4 class="item-title">Inovação:</h4>
+          <p class="text">
+            Trazer aos nossos clientes o que há de em tecnologia para Gestão de Pessoas.
+          </p>
+        </li>
+        <li class="item">
+          <figure class="icon">
+            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0)">
+                <path d="M13.7496 28.7304C6.16863 28.7304 0 22.562 0 14.981C0 7.39995 6.16863 1.23132 13.7496 1.23132C16.3858 1.23132 18.9506 1.98249 21.1668 3.40495C21.6019 3.68509 21.7294 4.26506 21.4492 4.70129C21.1682 5.13753 20.5893 5.26364 20.1531 4.98372C18.2406 3.7549 16.0258 3.10626 13.7496 3.10626C7.20223 3.10626 1.87494 8.43355 1.87494 14.981C1.87494 21.5282 7.20223 26.8554 13.7496 26.8554C20.2968 26.8554 25.6241 21.5282 25.6241 14.981C25.6241 14.5896 25.6054 14.2021 25.5692 13.821C25.5193 13.3047 25.8967 12.8472 26.4117 12.7973C26.9292 12.7497 27.3853 13.1248 27.4354 13.6398C27.4778 14.081 27.4991 14.5285 27.4991 14.981C27.4991 22.562 21.3307 28.7304 13.7496 28.7304Z" fill="black" />
+                <path d="M15.3118 18.1058C15.072 18.1058 14.8319 18.0145 14.6495 17.8309L9.02465 12.2061C8.65845 11.8397 8.65845 11.246 9.02465 10.8798C9.39085 10.5136 9.98455 10.5136 10.3507 10.8798L15.3132 15.8423L28.3989 2.75634C28.7653 2.39014 29.359 2.39014 29.7252 2.75634C30.0914 3.12254 30.0914 3.71624 29.7252 4.08244L15.9756 17.8321C15.7918 18.0145 15.5519 18.1058 15.3118 18.1058Z" fill="black" />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="30" height="30" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+
+          </figure>
+          <h4 class="item-title">Empreendedorismo:</h4>
+          <p class="text">
+            Pensamento de dono e protagonismo, fazem parte do nosso jeito.
+          </p>
+        </li>
+        <li class="item">
+          <figure class="icon">
+            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0)">
+                <path d="M13.7496 28.7304C6.16863 28.7304 0 22.562 0 14.981C0 7.39995 6.16863 1.23132 13.7496 1.23132C16.3858 1.23132 18.9506 1.98249 21.1668 3.40495C21.6019 3.68509 21.7294 4.26506 21.4492 4.70129C21.1682 5.13753 20.5893 5.26364 20.1531 4.98372C18.2406 3.7549 16.0258 3.10626 13.7496 3.10626C7.20223 3.10626 1.87494 8.43355 1.87494 14.981C1.87494 21.5282 7.20223 26.8554 13.7496 26.8554C20.2968 26.8554 25.6241 21.5282 25.6241 14.981C25.6241 14.5896 25.6054 14.2021 25.5692 13.821C25.5193 13.3047 25.8967 12.8472 26.4117 12.7973C26.9292 12.7497 27.3853 13.1248 27.4354 13.6398C27.4778 14.081 27.4991 14.5285 27.4991 14.981C27.4991 22.562 21.3307 28.7304 13.7496 28.7304Z" fill="black" />
+                <path d="M15.3118 18.1058C15.072 18.1058 14.8319 18.0145 14.6495 17.8309L9.02465 12.2061C8.65845 11.8397 8.65845 11.246 9.02465 10.8798C9.39085 10.5136 9.98455 10.5136 10.3507 10.8798L15.3132 15.8423L28.3989 2.75634C28.7653 2.39014 29.359 2.39014 29.7252 2.75634C30.0914 3.12254 30.0914 3.71624 29.7252 4.08244L15.9756 17.8321C15.7918 18.0145 15.5519 18.1058 15.3118 18.1058Z" fill="black" />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="30" height="30" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+
+          </figure>
+          <h4 class="item-title">Agilidade:</h4>
+          <p class="text">
+            Ser rápido em nossas entregas, mantendo a qualidade acima da média.
+          </p>
+        </li>
+        <li class="item">
+          <figure class="icon">
+            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0)">
+                <path d="M13.7496 28.7304C6.16863 28.7304 0 22.562 0 14.981C0 7.39995 6.16863 1.23132 13.7496 1.23132C16.3858 1.23132 18.9506 1.98249 21.1668 3.40495C21.6019 3.68509 21.7294 4.26506 21.4492 4.70129C21.1682 5.13753 20.5893 5.26364 20.1531 4.98372C18.2406 3.7549 16.0258 3.10626 13.7496 3.10626C7.20223 3.10626 1.87494 8.43355 1.87494 14.981C1.87494 21.5282 7.20223 26.8554 13.7496 26.8554C20.2968 26.8554 25.6241 21.5282 25.6241 14.981C25.6241 14.5896 25.6054 14.2021 25.5692 13.821C25.5193 13.3047 25.8967 12.8472 26.4117 12.7973C26.9292 12.7497 27.3853 13.1248 27.4354 13.6398C27.4778 14.081 27.4991 14.5285 27.4991 14.981C27.4991 22.562 21.3307 28.7304 13.7496 28.7304Z" fill="black" />
+                <path d="M15.3118 18.1058C15.072 18.1058 14.8319 18.0145 14.6495 17.8309L9.02465 12.2061C8.65845 11.8397 8.65845 11.246 9.02465 10.8798C9.39085 10.5136 9.98455 10.5136 10.3507 10.8798L15.3132 15.8423L28.3989 2.75634C28.7653 2.39014 29.359 2.39014 29.7252 2.75634C30.0914 3.12254 30.0914 3.71624 29.7252 4.08244L15.9756 17.8321C15.7918 18.0145 15.5519 18.1058 15.3118 18.1058Z" fill="black" />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="30" height="30" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+
+          </figure>
+          <h4 class="item-title">Respeito ao Ser Humano:</h4>
+          <p class="text">
+            Feedback aqui é uma regra, respeitamos o tempo de cada
+            pessoa, seja empresa ou candidato.
+          </p>
+        </li>
+        <li class="item">
+          <figure class="icon">
+            <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g clip-path="url(#clip0)">
+                <path d="M13.7496 28.7304C6.16863 28.7304 0 22.562 0 14.981C0 7.39995 6.16863 1.23132 13.7496 1.23132C16.3858 1.23132 18.9506 1.98249 21.1668 3.40495C21.6019 3.68509 21.7294 4.26506 21.4492 4.70129C21.1682 5.13753 20.5893 5.26364 20.1531 4.98372C18.2406 3.7549 16.0258 3.10626 13.7496 3.10626C7.20223 3.10626 1.87494 8.43355 1.87494 14.981C1.87494 21.5282 7.20223 26.8554 13.7496 26.8554C20.2968 26.8554 25.6241 21.5282 25.6241 14.981C25.6241 14.5896 25.6054 14.2021 25.5692 13.821C25.5193 13.3047 25.8967 12.8472 26.4117 12.7973C26.9292 12.7497 27.3853 13.1248 27.4354 13.6398C27.4778 14.081 27.4991 14.5285 27.4991 14.981C27.4991 22.562 21.3307 28.7304 13.7496 28.7304Z" fill="black" />
+                <path d="M15.3118 18.1058C15.072 18.1058 14.8319 18.0145 14.6495 17.8309L9.02465 12.2061C8.65845 11.8397 8.65845 11.246 9.02465 10.8798C9.39085 10.5136 9.98455 10.5136 10.3507 10.8798L15.3132 15.8423L28.3989 2.75634C28.7653 2.39014 29.359 2.39014 29.7252 2.75634C30.0914 3.12254 30.0914 3.71624 29.7252 4.08244L15.9756 17.8321C15.7918 18.0145 15.5519 18.1058 15.3118 18.1058Z" fill="black" />
+              </g>
+              <defs>
+                <clipPath id="clip0">
+                  <rect width="30" height="30" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+
+          </figure>
+          <h4 class="item-title">Ética com transparência:</h4>
+          <p class="text">
+            Não negociar nossos valores ou agir de forma diferente do nosso
+            código de conduta.
+          </p>
+        </li>
+      </ul>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
