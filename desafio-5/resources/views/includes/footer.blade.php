aa8859101bfda789cb0cf972ce67de540a17f785
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-07-12 15:30:13 -0300

    footer

--- desafio-5/resources/views/includes/footer.blade.php
+++ desafio-5/resources/views/includes/footer.blade.php
@@ -1,3 +1,389 @@
+<footer class="footer">
+  <div class="footer-top">
+    <div class="col">
+      <div class="group">
+        <h2 class="title">Anuncie</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">Anúncios web</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Midiaweb</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="group">
+        <h2 class="title">Assinatura</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">Clube AT</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Edição digital</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="group">
+        <h2 class="title">Classificados</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">Anúncios web</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Midiaweb</a>
+          </li>
+        </ul>
+      </div>
+
+      <a href="#" class="link-title">Expediente</a>
+      <a href="#" class="link-title">Política de privacidade</a>
+      <a href="#" class="link-title">Termos de uso</a>
+    </div>
+
+    <div class="col">
+      <div class="group">
+        <h2 class="title">Últimas Notícias</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">AT Carreiras</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Atualidades</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Cidades</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Ciência & Saúde</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Concurso & Emprego</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Economia</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Eleições 2020</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Natureza</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Passei por aqui</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Polícia</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Política</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Porto & Mar</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Turismo</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Vc na AT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="group">
+        <h2 class="title">Cidades</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">Bertioga</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Cubatão</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Guarujá</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Litoral Sul</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Praia Grande</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Santos</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">São Vicente</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Vale do Ribeira</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="group">
+        <h2 class="title">Variedades</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">AT Revista</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Boa Mesa</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Comportamento</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Games & Tecnologia</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Luiz Alca</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Programe-se</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Pop & Art</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="group">
+        <h2 class="title">Esportes</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">Corinthians</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Jabaquara</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Santos FC</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">São Paulo</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Palmeiras</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Portuguesa Santista</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Esporte regional</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Mais esporte</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="group">
+        <h2 class="title">Especiais</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">AT Ciência</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">AT Imóveis</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Nossa Senhora do M. Serrat</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Onde ir</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Revista Porto & Mar</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Porto & Mar</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Saúde AT</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Top of Mind</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="group">
+        <h2 class="title">Eventos</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">10 KM Tribuna FM-Unilus</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">A Região em Pauta</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">A Tribuna de Surf Colegial</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">A Tribuna de Tênis</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Arena Praia & Cia</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Atitude Verde</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Comunidade em Ação</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Congresso de Direito</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Marítimo e Portuário</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Corrida Virtual</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Envelheça Leve</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Porto Profissões</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Seminário da Indústria da Construção Civil</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Seminário Porto & Mar</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Top of Mind</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Tribuna Motor Show</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Vc na AT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="group">
+        <h2 class="title">Opinião</h2>
+        <ul class="list">
+          <li class="item">
+            <a href="#" class="link">Alexandre Catena Volpe</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Alexandre Lopes</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Ana Lucia M. Simão Cury</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Arminda Augusto</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Blog n'Roll</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Caio França</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Cida Coelho</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Dad Squarisi</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Direito Previdenciário</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Editorial A Tribuna</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Eduardo Silva</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Eu Estudo Certo</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Júnior Bozzella</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Kenny Mendes</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Márcia Atik</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Márcio Calves</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Maxwell Rodrigues</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Paulo Corrêa Jr.</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Resenha Esportiva</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Roberto Monteiro</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Rosana Valle</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Tenente Coimbra</a>
+          </li>
+          <li class="item">
+            <a href="#" class="link">Tribuna do leitor</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    <div class="left">
+      <div class="top">
+        <img src="{{asset('images/atribuna-logo.png')}}" alt="A Tribuna Logo" title="A Tribuna Logo">
+        <a href="#" class="assine-button">Assine</a>
+      </div>
+
+      <div class="bottom">
+        <span class="acompanhe">Acompanhe-nos</span>
+        <div class="icon">
+          <a href="#" class="icon-link" target="_blank">
+            <img src="{{asset('svg/facebook-purple.svg')}}" alt="Facebook | Cluebe A Tribuna" title="Facebook | Cluebe A Tribuna">
+          </a>
+        </div>
+        <div class="icon">
+          <a href="#" class="icon-link" target="_blank">
+            <img src="{{asset('svg/instagram-purple.svg')}}" alt="Instagram | Cluebe A Tribuna" title="Instagram | Cluebe A Tribuna">
+          </a>
+        </div>
+        <div class="icon">
+          <a href="#" class="icon-link" target="_blank">
+            <img src="{{asset('svg/twitter-purple.svg')}}" alt="Twitter | Cluebe A Tribuna" title="Twitter | Cluebe A Tribuna">
+          </a>
+        </div>
+        <div class="icon">
+          <a href="#" class="icon-link" target="_blank">
+            <img src="{{asset('svg/player-purple.svg')}}" alt="YouTube | Cluebe A Tribuna" title="YouTube | Cluebe A Tribuna">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
 <script src="{{asset('/js/main-min.js')}}"></script>
 
