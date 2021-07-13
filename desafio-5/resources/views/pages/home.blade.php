c2125c61b4c186e6b8e246a915a820d2f0254a75
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-07-12 21:34:37 -0300

    finalizando mobile

--- desafio-5/resources/views/pages/home.blade.php
+++ desafio-5/resources/views/pages/home.blade.php
@@ -7,43 +7,45 @@
 <main class="home-page">
   <section class="banner">
     <img src="{{supportsWebp() ? asset('images/banner.webp') : asset('images/banner.png')}}" alt="Clube de Assinantes A Tribuna" title="Clube de Assinantes A Tribuna" class="banner-image">
-    <h2 class="title">O Maior clube de vantagens da região</h2>
-    <h3 class="subtitle">Com o que você <span class="line">gasta</span> no mês?</h3>
+    <div class="banner-content">
+      <h2 class="title">O Maior clube de vantagens da região</h2>
+      <h3 class="subtitle">Com o que você <span class="line">gasta</span> no mês?</h3>
 
-    <div class="row">
-      <div class="col left">
-        <img class="icon" src="{{asset('/svg/cutlery.svg')}}" alt="cutlery">
+      <div class="row">
+        <div class="col left">
+          <img class="icon" src="{{asset('/svg/cutlery.svg')}}" alt="cutlery">
 
-        <div class="texts">
-          <span class="text"><b class="price">R$ 100</b> <b class="bold">restaurante</b></span>
-          <span class="text percentage"><b class="price">20%</b> Desconto do clube</span>
-          <span class="text"><b class="price">= R$ 20</b> de economia</span>
+          <div class="texts">
+            <span class="text"><b class="price">R$ 100</b> <b class="bold">restaurante</b></span>
+            <span class="text percentage"><b class="price">20%</b> Desconto do clube</span>
+            <span class="text"><b class="price">= R$ 20</b> de economia</span>
+          </div>
+        </div>
+
+        <div class="col middle">
+          <img class="icon" src="{{asset('/svg/gas-pump.svg')}}" alt="gas-pump">
+
+          <div class="texts">
+            <span class="text"><b class="price">R$ 200</b> <b class="bold">gasolina</b></span>
+            <span class="text percentage"><b class="price">5%</b> Desconto do clube</span>
+            <span class="text"><b class="price">= R$ 10</b> de economia</span>
+          </div>
+        </div>
+
+        <div class="col right">
+          <img class="icon" src="{{asset('/svg/washing-machine.svg')}}" alt="washing-machine">
+
+          <div class="texts">
+            <span class="text"><b class="price">R$ 100</b> <b class="bold">lavanderia</b></span>
+            <span class="text percentage"><b class="price">20%</b> Desconto do clube</span>
+            <span class="text"><b class="price">= R$ 20</b> de economia</span>
+          </div>
         </div>
       </div>
 
-      <div class="col middle">
-        <img class="icon" src="{{asset('/svg/gas-pump.svg')}}" alt="gas-pump">
-
-        <div class="texts">
-          <span class="text"><b class="price">R$ 200</b> <b class="bold">gasolina</b></span>
-          <span class="text percentage"><b class="price">5%</b> Desconto do clube</span>
-          <span class="text"><b class="price">= R$ 10</b> de economia</span>
-        </div>
-      </div>
-
-      <div class="col right">
-        <img class="icon" src="{{asset('/svg/washing-machine.svg')}}" alt="washing-machine">
-
-        <div class="texts">
-          <span class="text"><b class="price">R$ 100</b> <b class="bold">lavanderia</b></span>
-          <span class="text percentage"><b class="price">20%</b> Desconto do clube</span>
-          <span class="text"><b class="price">= R$ 20</b> de economia</span>
-        </div>
-      </div>
+      <a href="#" class="button">clique aqui e Faça parte do clube</a>
+      <span class="helper">Sua economia pode pagar a sua Assinatura!</span>
     </div>
-
-    <a href="#" class="button">clique aqui e Faça parte do clube</a>
-    <span class="helper">Sua economia pode pagar a sua Assinatura!</span>
   </section>
 
   <section class="phone">
@@ -183,7 +185,7 @@
           </li>
         </ul>
       </div>
-      <div data-glide-el="controls">
+      <div data-glide-el="controls" class="glide__controls">
         <button data-glide-dir="<" class="arrow arrow-left"><i data-feather="chevron-left"></i></button>
         <button data-glide-dir=">" class="arrow arrow-right"><i data-feather="chevron-right"></i></button>
       </div>
