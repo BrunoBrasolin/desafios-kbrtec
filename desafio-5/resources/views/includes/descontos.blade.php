0a86e31a4c4d0e79b6b7f46545ca586f935faad1
Bruno Brasolin <50347981+BrunoBrasolin@users.noreply.github.com>
2021-07-21 10:37:36 -0300

    hover finalizado

--- desafio-5/resources/views/includes/descontos.blade.php
+++ desafio-5/resources/views/includes/descontos.blade.php
@@ -14,37 +14,37 @@
         <ul class="glide__slides">
           <li class="glide__slide">
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-1.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 5% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-2.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 8% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-3.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 10% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-4.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 3% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-5.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 4% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-6.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 10% de volta</p>
@@ -52,37 +52,37 @@
           </li>
           <li class="glide__slide">
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-1.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 5% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-2.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 8% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-3.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 10% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-4.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 3% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-5.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 4% de volta</p>
             </div>
             <div class="desconto-item">
-              <figure class="logo-contaienr">
+              <figure class="logo-container">
                 <img src="{{asset('/images/logos/logo-6.png')}}" alt="alt" title="title">
               </figure>
               <p class="desc">Ganhe 10% de volta</p>
